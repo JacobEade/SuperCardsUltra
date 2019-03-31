621c0588d491a80a7b908a51788f3725694fae26
Jacob Eade <38705400+Pompey-the-Great@users.noreply.github.com>
2019-03-31 11:18:35 -0500

    updated class diagram and updated player class

--- SuperCardsLib/Player.cs
+++ SuperCardsLib/Player.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace SuperCardsLib
 {
     public class Player
     {
         List<Card> hand;
-        public Player(string username)
+        public Player(string username,int wins, int losses)
         {
             Username = username;
             hand = new List<Card>();
+            Wins = wins;
+            Losses = losses;
         }
 
+        public int Losses { get; private set; }
         public string Username { get; private set; }
+        public int Wins { get; private set; }
     }
 }
