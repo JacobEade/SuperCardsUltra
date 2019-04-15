50808704e588a05da11f2807dd20cbd4ff39ac2c
HarrisonWelch <hwelch1@my.apsu.edu>
2019-04-15 04:15:45 -0500

    fixed comments on Viewable

--- SuperCardsLib/Viewable.cs
+++ SuperCardsLib/Viewable.cs
@@ -5,6 +5,7 @@ namespace SuperCardsLib
     // Interface: Viewable - Thing that is viewed
     public interface Viewable
     {
+        // Ivar: GetImageNames - the image names for the cards
         List<string> GetImageNames();
     }
 }
